b5d71a9373c5c78c21c0f0ecdad295c46abd73c9
abejgonzalez <abe.j.gonza@gmail.com>
2023-10-13 16:34:11 -0700

    Temp. use hello.c to test R/W into other mem. space

--- tests/hello.c
+++ tests/hello.c
@@ -6,5 +6,18 @@ int main(void) {
   uint64_t marchid = read_csr(marchid);
   const char* march = get_march(marchid);
   printf("Hello world from core 0, a %s\n", march);
+
+  printf("Attempting to read from smartNICaddr\n");
+
+  uint64_t* smartNICaddr = 0xa0000000UL;
+  *smartNICaddr = 0xdeadbeef;
+
+  printf("Wrote to smartNIC address\n");
+
+  uint64_t tmp = -1;
+  tmp = *smartNICaddr;
+
+  printf("Read from smartNIC address = %lu\n", tmp);
+
   return 0;
 }
