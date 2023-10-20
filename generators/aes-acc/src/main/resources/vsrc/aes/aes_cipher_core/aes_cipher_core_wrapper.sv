cdb07059b0dce9d3e64661fb47f145deffef2d98
abejgonzalez <abe.j.gonza@gmail.com>
2023-10-20 16:43:40 -0700

    Add more AES specific items

---
+++ generators/aes-acc/src/main/resources/vsrc/aes/aes_cipher_core/aes_cipher_core_wrapper.sv
@@ -0,0 +1,95 @@
+module aes_cipher_core_wrapper
+  input  logic                        clk_i,
+  input  logic                        rst_ni,
+
+  // Input handshake signals
+  input  logic                       in_valid_i,
+  output logic                       in_ready_o,
+
+  // Output handshake signals
+  output logic                       out_valid_o,
+  input  logic                       out_ready_i,
+
+  // Control and sync signals
+  input  logic                  [1:0] op_i,
+  input  logic                  [2:0] key_len_i,
+  input  logic                        crypt_i,
+  input  logic                        dec_key_gen_i,
+  input  logic                        prng_reseed_i,
+  output logic                        alert_o,
+
+  // Pseudo-random data for register clearing
+  input  logic                 [63:0] prd_clearing_i_0,
+  input  logic                 [63:0] prd_clearing_i_0,
+
+  // Masking PRNG
+  input  logic                        force_masks_i, // Useful for SCA only.
+  output logic                [127:0] data_in_mask_o,
+  output logic                        entropy_req_o,
+  input  logic                        entropy_ack_i,
+  input  logic                 [31:0] entropy_i,
+
+  // I/O data & initial key
+  input  logic                [127:0] state_init_i_0,
+  input  logic                [127:0] state_init_i_1,
+  input  logic                [255:0] key_init_i_0,
+  input  logic                [255:0] key_init_i_1,
+  output logic                [127:0] state_o_0,
+  output logic                [127:0] state_o_1
+);
+
+  import aes_pkg::*;
+
+  localparam bit          SecMasking   = 1;
+  localparam sbox_impl_e  SecSBoxImpl  = SecMasking ? SBoxImplDom : SBoxImplCanright;
+
+  assign in_valid_i_conv = in_valid_i ? SP2V_HIGH : SP2V_LOW;
+  assign out_ready_i_conv = out_ready_i ? SP2V_HIGH : SP2V_LOW;
+  assign in_ready_o = (in_ready_o_conv == SP2V_HIGH) ? '1 : '0;
+  assign out_valid_o = (out_valid_o_conv == SP2V_HIGH) ? '1 : '0;
+  assign crypt_i_conv = crypt_i ? SP2V_HIGH : SP2V_LOW;
+  assign dec_key_gen_i_conv = dec_key_gen_i ? SP2V_HIGH : SP2V_LOW;
+
+  aes_cipher_core #(
+    .SecMasking  ( SecMasking  ),
+    .SecSBoxImpl ( SecSBoxImpl )
+  ) u_aes_cipher_core (
+    .clk_i            ( clk_i             ),
+    .rst_ni           ( rst_ni            ),
+
+    .in_valid_i       ( in_valid_i_conv   ),
+    .in_ready_o       ( in_ready_o_conv   ),
+
+    .out_valid_o      ( out_valid_o_conv  ),
+    .out_ready_i      ( out_ready_i_conv  ),
+
+    .cfg_valid_i      ( 1'b1              ), // Used for gating assertions only.
+    .op_i             ( op_i              ),
+    .key_len_i        ( key_len_i         ),
+    .crypt_i          ( crypt_i_conv      ),
+    .crypt_o          (                   ), // Ignored.
+    .dec_key_gen_i    ( dec_key_gen_i_conv),
+    .dec_key_gen_o    (                   ), // Ignored.
+    .prng_reseed_i    ( prng_reseed_i     ),
+    .prng_reseed_o    (                   ), // Ignored.
+    .key_clear_i      ( 1'b0              ), // Ignored.
+    .key_clear_o      (                   ), // Ignored.
+    .data_out_clear_i ( 1'b0              ), // Ignored.
+    .data_out_clear_o (                   ), // Ignored.
+    .alert_fatal_i    ( 1'b0              ), // Ignored.
+    .alert_o          ( alert_o           ), // Ignored.
+
+    .prd_clearing_i   ( {prd_clearing_i_1, prd_clearing_i_1} ),
+
+    .force_masks_i    ( 1'b0              ), // Ignored.
+    .data_in_mask_o   ( data_in_mask_o    ),
+    .entropy_req_o    ( entropy_req_o     ),
+    .entropy_ack_i    ( entropy_ack_i     ),
+    .entropy_i        ( entropy_i         ),
+
+    .state_init_i     ( {state_init_i_1, state_init_i_0}      ),
+    .key_init_i       ( {key_init_i_1, key_init_i_0}        ),
+    .state_o          ( {state_o_i_1, state_o_i_0}         )
+  );
+
+endmodule
--- generators/aes-acc/src/main/scala/aes/AesCipherCore.scala
+++ generators/aes-acc/src/main/scala/aes/AesCipherCore.scala
@@ -1,275 +1,230 @@
-//package aes
-//
-//import chisel3._
-//import chisel3.util._
-//import chisel3.util.random.{LFSR}
-//import chisel3.experimental.{IntParam, BaseModule}
-//import org.chipsalliance.cde.config.{Field, Parameters}
-//import freechips.rocketchip.subsystem.{BaseSubsystem, PeripheryBusKey}
-//import freechips.rocketchip.diplomacy._
-//import freechips.rocketchip.regmapper._
-//import freechips.rocketchip.tilelink._
+package aes
 
-//// encrypt/decrypt 128b of data (16B) blocks with a 256b key
-//class aes_cipher_core extends BlackBox with HasBlackBoxResource {
-//  // TODO: fix bundle
-//  val io = IO(new Bundle {
-//    val clk_i = Input(Clock())
-//    val rst_ni = Input(Reset())
-//
-//    val in_valid_i = Input(Bool())
-//    val in_ready_o = Output(Bool())
-//
-//    val out_valid_o = Output(Bool())
-//    val out_ready_i = Input(Bool())
-//
-//    val op_i = Input(UInt(2.W))
-//    val key_len_i = Input(UInt(3.W))
-//    val crypt_i = Input(Bool())
-//    val dec_key_gen_i = Input(Bool()) // decrypt key gen?
-//    val prng_reseed_i = Input(Bool())
-//
-//    val prd_clearing_i_0 = Input(UInt(64.W))
-//    val prd_clearing_i_1 = Input(UInt(64.W))
-//
-//    val data_in_mask_o = Output(UInt(128.W))
-//    val entropy_req_o = Output(Bool())
-//    val entropy_ack_i = Input(Bool())
-//    val entropy_i = Input(UInt(32.W))
-//
-//    val state_init_i_0 = Input(UInt(128.W))
-//    val state_init_i_1 = Input(UInt(128.W))
-//    val key_init_i_0 = Input(UInt(256.W))
-//    val key_init_i_1 = Input(UInt(256.W))
-//    val state_o_0 = Output(UInt(128.W))
-//    val state_o_1 = Output(UInt(128.W))
-//  })
-//
-//  def in_fire() = io.in_valid_i && io.in_ready_o
-//  def out_fire() = io.out_valid_o && io.out_ready_i
-//
-//  addResource("/vsrc/aes_cipher_core/aes_cipher_core.sv")
-//  // TODO: add more
-//}
-//
-//object AesCipherCoreConsts {
-//  val AES_256 = "b100".U
-//
-//  val CIPH_FWD = "b01".U
-//  val CIPH_INV = "b10".U
-//}
+import sys.process._
 
-//class InCryptBundle extends Bundle {
-//  val encrypt = Input(Bool()) // if not then decrypt
-//  val data = Input(UInt(128.W))
-//}
-//
-//class OutCryptBundle extends Bundle {
-//  val data = Output(UInt(128.W))
-//}
-//
-//class AesCipherCoreDriver extends Module {
-//  val io = IO(new Bundle {
-//    val in = DecoupledIO(new InCryptBundle)
-//    val out = DecoupledIO(new OutCryptBundle)
-//  })
-//
-//  val acc = Module(new AesCipherCore)
-//  acc.io.key_len_i := AesCipherCoreConsts.AES_256
-//  acc.io.out_ready_i := true.B // TODO: this TB is always high?
-//  acc.io.entropy_ack_i := true.B // entropy is always available
-//  acc.io.entropy_i := LFSR(32, acc.io.entropy_req_o)
-//  acc.io.prd_clearing_i_0 := LFSR(64, acc.io.out_valid_o)
-//  acc.io.prd_clearing_i_1 := LFSR(64, acc.io.out_valid_o)
-//  val scalaRand = new scala.util.Random
-//  acc.io.key_init_i_0 := (rand.nextInt()).U
-//  acc.io.key_init_i_1 := (rand.nextInt()).U
-//
-//  val INITIAL_IDLE :: INIT_RESEED :: ENCRYPT :: DECRYPT :: Nil = Enum(8)
-//  val state = RegInit(INITIAL_IDLE)
-//
-//  val op = RegInit(AesCipherCoreConsts.CIPH_FWD)
-//  val prng_reseed = RegInit(false.B)
-//
-//  val reseed_helper = DecoupledHelper(
-//    state === INIT_RESEED
-//  )
-//
-//  val encrypt_helper = DecoupledHelper(
-//    state === ENCRYPT,
-//    io.in.valid
-//  )
-//
-//  val decrypt_helper = DecoupledHelper(
-//    state === DECRYPT,
-//    io.in.valid
-//  )
-//
-//  acc.io.in_valid_i := reseed_helper.fire() || encrypt_helper.fire() || decrypt_helper.fire() || decrypt_key_gen_helper.fire()
-//  acc.io.crypt_i := !reseed_helper.fire()
-//  acc.io.prng_reseed_i := true.B
-//  acc.io.state_init_i_0 := io.in.bits.data ^ acc.io.data_in_mask_o
-//  acc.io.state_init_i_1 := io.in.bits.data ^ acc.io.data_in_mask_o
-//  acc.io.op_i := Mux(encrypt_helper.fire(), AesCipherCoreConsts.CIPH_FWD, Mux(decrypt_helper.fire(), AesCipherCoreConsts.CIPH_INV, 0.U))
-//  io.out.bits.data := acc.io.state_o_1 ^ acc.io.state_o_0
-//  io.out.valid := (state === ENCRYPT || state === DECRYPT) && acc.io.out_valid_o
-//  io.in.ready := acc.io.in_ready_o
-//
-//  assert(!(encrypt_helper.fire() && decrypt_helper.fire()), "Shouldn't be able to decrypt/encrypt at same time")
-//
-//  // wait for core to be ready
-//  when (state === INITIAL_IDLE && acc.io.in_ready_o) {
-//    state := INIT_RESEED
-//  }
-//
-//  // initially preseed the rng
-//  when (state === INIT_RESEED && acc.io.out_valid_o) {
-//    state := IDLE
-//  }
-//
-//  when (state === IDLE && io.crypt.valid) {
-//    state := Mux(io.crypt.bits.encrypt, ENCRYPT, DEC_KEY_GEN)
-//  }
-//
-//  when (state === DECRYPT) {
-//
-//
-//  }
-//
-//  switch(state) {
-//    is(DEC_KEY_GEN) {
-//      acc.io.in_valid_i := true.B
-//      acc.io.dec_key_gen_i := true.B
-//      acc.io.prng_reseed_i := true.B
-//
-//      when (acc.io.out_valid_o) {
-//        state := ???
-//      }
-//    }
-//
-//    is(DECRYPT) {
-//      when (acc.io.out_valid_o) {
-//        val encrypted_data = acc.io.state_o_1 ^ acc.io.state_o_0
-//        state := ???
-//      }
-//    }
-//  }
-//}
-//
-////   // DUT signals
-////   sp2v_e                       in_ready, in_valid, out_valid;
-////   ciph_op_e                    op;
-////   key_len_e                    key_len_d, key_len_q;
-////   sp2v_e                       crypt, dec_key_gen;
-////   logic                        prng_reseed;
-////   logic [WidthPRDClearing-1:0] prd_clearing [NumShares];
-////   logic        [3:0][3:0][7:0] state_mask;
-////   logic        [3:0][3:0][7:0] state_init [NumShares];
-////   logic        [3:0][3:0][7:0] state_done [NumShares];
-////   logic            [7:0][31:0] key_init [NumShares];
-////
-////   // Instantiate DUT
-////   aes_cipher_core #(
-////     .in_valid_i       ( in_valid            ),
-////     .in_ready_o       ( in_ready            ),
-////     .out_valid_o      ( out_valid           ),
-////     .op_i             ( op                  ),
-////     .key_len_i        ( key_len_q           ),
-////     .crypt_i          ( crypt               ),
-////     .dec_key_gen_i    ( dec_key_gen         ),
-////     .prng_reseed_i    ( prng_reseed         ),
-////     .data_in_mask_o   ( state_mask          ),
-////     .state_init_i     ( state_init          ),
-////     .key_init_i       ( key_init            ),
-////     .state_o          ( state_done          )
-////   );
-////   logic                 data_in_buf_we, data_out_buf_we, check, mismatch, test_done;
-////   logic [3:0][3:0][7:0] data_in_rand, data_in;
-////   logic [3:0][3:0][7:0] data_out;
-////   logic [3:0][3:0][7:0] data_in_buf[256];
-////   logic [3:0][3:0][7:0] data_out_buf[256];
-////
-////   // Generate the initial state.
-////   if (!SecMasking) begin : gen_state_init_no_masking
-////     // Only Share 0 is used.
-////     assign state_init[0] = data_in;
-////
-////     // Tie-off unused signals.
-////     logic unused_bits;
-////     assign unused_bits = ^state_mask;
-////   end else begin : gen_state_init_masking
-////     // Mask the input data with the mask provided by the internal masking PRNG.
-////     assign state_init[0] = data_in ^ state_mask;
-////     assign state_init[1] = state_mask;
-////   end
-////
-////   always_comb begin : aes_cipher_core_tb_fsm
-////     // DUT
-////     in_valid    = SP2V_LOW;
-////     op          = CIPH_FWD;
-////     crypt       = SP2V_HIGH;
-////     dec_key_gen = SP2V_LOW;
-////     prng_reseed = 1'b0;
-////
-////     // TB
-////     aes_cipher_core_tb_state_d = aes_cipher_core_tb_state_q;
-////     block_count_increment      = 1'b0;
-////     block_count_clear          = 1'b0;
-////     key_len_d                  = key_len_q;
-////     data_in_buf_we             = 1'b0;
-////     data_out_buf_we            = 1'b0;
-////     check                      = 1'b0;
-////     test_done                  = 1'b0;
-////
-////     unique case (aes_cipher_core_tb_state_q)
-////
-////       IDLE: begin
-////         // Just wait for the ciphre core to become ready.
-////         if (in_ready == SP2V_HIGH) begin
-////           aes_cipher_core_tb_state_d = SecMasking ? INIT_RESEED : ECB_ENCRYPT;
-////         end
-////       end
-////
-////       INIT_RESEED: begin
-////         // Perform an initial reseed of the internal masking PRNG to put it into a random state.
-////         in_valid    = SP2V_HIGH;
-////         crypt       = SP2V_LOW;
-////         prng_reseed = 1'b1;
-////         if (out_valid == SP2V_HIGH) begin
-////           aes_cipher_core_tb_state_d = ECB_ENCRYPT;
-////         end
-////       end
-////
-////       ECB_ENCRYPT: begin
-////         // Perform encryption in parallel with a reseed of the internal masking PRNG.
-////         in_valid    = SP2V_HIGH;
-////         prng_reseed = 1'b1;
-////         if (out_valid == SP2V_HIGH) begin
-////           block_count_increment = 1'b1;
-////           data_in_buf_we  = 1'b1;
-////           data_out_buf_we = 1'b1;
-////           // Increase the key length after every 8 blocks.
-////           key_len_d = (block_count_q == 8'd7)  ? AES_192 :
-////                       (block_count_q == 8'd15) ? AES_256 : key_len_q;
-////           // After 24 blocks, we're starting over with decryption.
-////           if (block_count_q == 8'd23) begin
-////             block_count_clear          = 1'b1;
-////             key_len_d                  = AES_128;
-////             aes_cipher_core_tb_state_d = DEC_KEY_GEN;
-////           end
-////         end
-////       end
-////
-////       DEC_KEY_GEN: begin
-////         // Perform encryption in parallel with a reseed of the internal masking PRNG.
-////         in_valid    = SP2V_HIGH;
-////         dec_key_gen = SP2V_HIGH;
-////         prng_reseed = 1'b1;
-////         if (out_valid == SP2V_HIGH) begin
-////           aes_cipher_core_tb_state_d = ECB_DECRYPT;
-////         end
-////       end
-////     endcase
-////   end
-////
-//// endmodule
+import chisel3._
+import chisel3.util._
+import chisel3.util.random.{LFSR}
+import chisel3.experimental.{IntParam, BaseModule}
+
+import org.chipsalliance.cde.config.{Field, Parameters}
+
+import freechips.rocketchip.subsystem.{BaseSubsystem, PeripheryBusKey}
+import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.regmapper._
+import freechips.rocketchip.tilelink._
+
+// general resources used:
+//   https://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-38a.pdf
+//   https://github.com/lowRISC/opentitan/blob/fe702b60582f7c4e5549352a09e7992544d41bec/hw/ip/aes/rtl/aes_core.sv
+//   https://github.com/lowRISC/opentitan/blob/fe702b60582f7c4e5549352a09e7992544d41bec/hw/ip/aes/rtl/aes_control_fsm.sv
+//   https://github.com/lowRISC/opentitan/blob/fe702b60582f7c4e5549352a09e7992544d41bec/hw/ip/aes/rtl/aes_reg_top.sv
+//   https://github.com/lowRISC/opentitan/blob/fe702b60582f7c4e5549352a09e7992544d41bec/sw/device/lib/crypto/drivers/aes.c
+//   https://github.com/lowRISC/opentitan/blob/fe702b60582f7c4e5549352a09e7992544d41bec/sw/device/lib/crypto/drivers/aes_test.c
+
+// encrypt/decrypt 128b of data (16B) blocks with a 256b key
+class aes_cipher_core extends BlackBox with HasBlackBoxPath {
+  val io = IO(new Bundle {
+    val clk_i = Input(Clock())
+    val rst_ni = Input(Reset())
+
+    val in_valid_i = Input(Bool())
+    val in_ready_o = Output(Bool())
+
+    val out_valid_o = Output(Bool())
+    val out_ready_i = Input(Bool())
+
+    val op_i = Input(UInt(2.W))
+    val key_len_i = Input(UInt(3.W))
+    val crypt_i = Input(Bool())
+    val dec_key_gen_i = Input(Bool()) // decrypt key gen?
+    val prng_reseed_i = Input(Bool())
+
+    val prd_clearing_i_0 = Input(UInt(64.W))
+    val prd_clearing_i_1 = Input(UInt(64.W))
+
+    val data_in_mask_o = Output(UInt(128.W))
+    val entropy_req_o = Output(Bool())
+    val entropy_ack_i = Input(Bool())
+    val entropy_i = Input(UInt(32.W))
+
+    val state_init_i_0 = Input(UInt(128.W))
+    val state_init_i_1 = Input(UInt(128.W))
+    val key_init_i_0 = Input(UInt(256.W))
+    val key_init_i_1 = Input(UInt(256.W))
+    val state_o_0 = Output(UInt(128.W))
+    val state_o_1 = Output(UInt(128.W))
+  })
+
+  def in_fire() = io.in_valid_i && io.in_ready_o
+  def out_fire() = io.out_valid_o && io.out_ready_i
+
+  val chipyardDir = System.getProperty("user.dir")
+  val aesDir = s"$chipyardDir/generators/aes-acc/src/main/resources/vsrc/aes/aes_cipher_core"
+
+  val proc = s"make -C $aesDir core"
+  require(proc.! == 0, "Failed to run pre-processing step")
+
+  addPath(s"$aesDir/core.sv")
+}
+
+class InCryptBundle extends Bundle {
+  val encrypt = Input(Bool()) // if not then decrypt
+  val data = Input(UInt(128.W))
+  val key = Input(UInt(256.W))
+}
+
+class OutCryptBundle extends Bundle {
+  val data = Output(UInt(128.W))
+}
+
+// ECB-mode AES-256 block driver (expects the key to stay the same throughout the entire time of an encrypt/decrypt "chain")
+class AesCipherCoreDriver extends Module {
+  val io = IO(new Bundle {
+    val in = DecoupledIO(new InCryptBundle)
+    val out = DecoupledIO(new OutCryptBundle)
+  })
+
+  object AesCipherCoreConsts {
+    val AES_256 = "b100".U
+
+    val CIPH_FWD = "b01".U
+    val CIPH_INV = "b10".U
+  }
+
+  val acc = Module(new aes_cipher_core)
+  acc.io.clk_i := clock
+  acc.io.rst_ni := !reset.asBool
+
+  acc.io.key_len_i := AesCipherCoreConsts.AES_256
+  acc.io.entropy_ack_i := true.B // entropy is always available
+  acc.io.entropy_i := LFSR(32, acc.io.entropy_req_o)
+  acc.io.prd_clearing_i_0 := LFSR(64, acc.io.out_valid_o)
+  acc.io.prd_clearing_i_1 := LFSR(64, acc.io.out_valid_o)
+
+  val INITIAL_IDLE :: INIT_RESEED :: IDLE :: ENCRYPT :: DEC_KEY_GEN :: DECRYPT :: Nil = Enum(6)
+  val state = RegInit(INITIAL_IDLE)
+
+  switch (state) {
+    // wait for core to be ready
+    is (INITIAL_IDLE) {
+      when (acc.io.in_ready_o) {
+        state := INIT_RESEED
+      }
+    }
+
+    // initially preseed the rng
+    is (INIT_RESEED) {
+      when (acc.out_fire()) {
+        state := IDLE
+      }
+    }
+
+    // start here when switching from encrypt to decrypt
+    is (IDLE) {
+      when (io.in.valid) {
+        state := Mux(io.in.bits.encrypt, ENCRYPT, DEC_KEY_GEN)
+      }
+    }
+
+    is (ENCRYPT) {
+      when (io.in.valid) {
+        when (io.in.bits.encrypt) {
+          state := ENCRYPT
+        } .otherwise {
+          state := IDLE
+        }
+      } .otherwise {
+        state := IDLE
+      }
+    }
+
+    // create initial decryption key
+    is (DEC_KEY_GEN) {
+      when (acc.out_fire()) {
+        state := DECRYPT
+      }
+    }
+
+    is (DECRYPT) {
+      when (io.in.valid) {
+        when (!io.in.bits.encrypt) {
+          state := DECRYPT
+        } .otherwise {
+          state := IDLE
+        }
+      } .otherwise {
+        state := IDLE
+      }
+    }
+  }
+
+  // INIT_RESEED:
+  //   in_fire := true
+  //   crypt := false
+  //   prng_reseed := 1
+  // ENCRYPT_SEND:
+  //   in_fire := true
+  //   prng_reseed := 1
+  //   state_init_0 := in_data ^ data_in_mask_0
+  //   state_init_1 := data_in_mask_0
+  // ENCRYPT_RECV:
+  //   out_fire := true
+  //   io.out.bits.data := acc.io.state_o_1 ^ acc.io.state_o_0
+  // DEC_KEY_GEN_SEND: // have to generate the start key for decryption before it always
+  //   in_fire := true
+  //   dec_key_gen := true
+  //   prng_reseed := 1
+  // DEC_KEY_GEN_RECV:
+  //   out_fire := true
+  // DECRYPT_SEND:
+  //   in_fire := true
+  //   op := decrypt
+  //   prng_reseed := 1
+  //   state_init_0 := in_data ^ data_in_mask_0
+  //   state_init_1 := data_in_mask_0
+  // ENCRYPT_RECV:
+  //   out_fire := true
+  //   io.out.bits.data := acc.io.state_o_1 ^ acc.io.state_o_0
+
+  val op = RegInit(AesCipherCoreConsts.CIPH_FWD)
+  val prng_reseed = RegInit(false.B)
+  // kinda matches how this key is setup: https://github.com/lowRISC/opentitan/blob/fe702b60582f7c4e5549352a09e7992544d41bec/sw/device/lib/crypto/drivers/aes_test.c#L67
+  acc.io.key_init_i_0 := io.in.key
+  acc.io.key_init_i_1 := (-1).U
+
+  val encrypt_send_helper = DecoupledHelper(
+    state === ENCRYPT,
+    io.in.valid,
+    io.in.ready
+  )
+
+  val decrypt_send_helper = DecoupledHelper(
+    state === DECRYPT,
+    io.in.valid,
+    io.in.ready
+  )
+
+  when (io.in.fire) {
+    printf(":CMDIN: Encrypt(0x%x) Data(0x%x) Key(0x%x)\n", io.in.bits.encrypt, io.in.bits.data, io.in.bits.key)
+  }
+
+  when (io.out.fire) {
+    printf(":RESPOUT: Data(0x%x)\n", io.out.bits.data)
+  }
+
+  acc.io.in_valid_i := (state === INIT_RESEED) || encrypt_send_helper.fire(io.in.ready) || decrypt_send_helper.fire(io.in.ready)
+  io.in.ready := (acc.io.in_ready_o && state =/= INITIAL_IDLE)
+
+  io.out.valid := ((state === ENCRYPT) || (state === DECRYPT)) && acc.io.out_valid_o
+  acc.io.out_ready_i := io.out.ready || (state === DEC_KEY_GEN)
+  io.out.bits.data := acc.io.state_o_1 ^ acc.io.state_o_0
+
+  acc.io.crypt_i := state =/= INIT_RESEED
+  acc.io.prng_reseed_i := true.B // reseed as much as possible?
+
+  acc.io.state_init_i_0 := io.in.bits.data ^ acc.io.data_in_mask_o
+  acc.io.state_init_i_1 := io.in.bits.data ^ acc.io.data_in_mask_o
+
+  acc.io.op_i := Mux(state === ENCRYPT, AesCipherCoreConsts.CIPH_FWD, AesCipherCoreConsts.CIPH_INV)
+}
